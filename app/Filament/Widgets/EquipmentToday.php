fde53bb70c0375017c3a5954fe163b3072baf703
Fatan Akbar Yogenta <fatan.dev@gmail.com>
2024-08-09 06:08:51 +0700

    qweasd

--- app/Filament/Widgets/EquipmentToday.php
+++ app/Filament/Widgets/EquipmentToday.php
@@ -20,7 +20,7 @@ class EquipmentToday extends BaseWidget
         return $table
             ->query(
                 PerawatanEquipment::query()
-                    ->where('tgl_perawatan', Carbon::now()->toDateString())
+                    ->where('tgl_perawatan', Carbon::today()->toDateString())
                     ->orderBy('tgl_perawatan', 'asc')
             )
             ->defaultPaginationPageOption(5)
